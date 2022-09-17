6d261fc6ae724be9f1c7e9fd27ddf60399e70454
Egor <egor.mixajlichenko@gmail.com>
2022-09-17 13:22:49 +0300

    first lab

---
+++ src/src/A/AMain.java
@@ -0,0 +1,54 @@
+package A;
+
+import jade.core.AID;
+import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.domain.AMSService;
+import jade.domain.FIPAAgentManagement.AMSAgentDescription;
+import jade.domain.FIPAAgentManagement.SearchConstraints;
+import jade.domain.FIPAException;
+import jade.lang.acl.ACLMessage;
+
+import java.util.Random;
+
+public class AMain extends Agent {
+    @Override
+    protected void setup() {
+        System.out.println("Hello from agent A: " + getAID().getName() + "Ready!");
+        addBehaviour(new CyclicBehaviour(this) {
+            @Override
+            public void action() {
+                ACLMessage receivedMessage = receive();
+
+                if (receivedMessage != null) {
+                    System.out.println(" - " + myAgent.getLocalName() + " receive: " + receivedMessage.getContent());
+                }
+
+                block();
+            }
+        });
+
+        AMSAgentDescription[] agents = null;
+
+        try {
+            SearchConstraints c = new SearchConstraints();
+            c.setMaxResults((long) -1);
+            agents = AMSService.search(this, new AMSAgentDescription(), c);
+        } catch (FIPAException e) {
+            System.out.println("oops not found AMS: " + e);
+            e.printStackTrace();
+        }
+
+        assert agents != null;
+        for (AMSAgentDescription agent : agents) {
+            AID agentId = agent.getName();
+            ACLMessage message = new ACLMessage(ACLMessage.INFORM);
+            message.addReceiver(agentId);
+            message.setLanguage("English");
+            int rand = new Random().nextInt(7 - 1) + 1;
+            String messageContent = rand == 3 ? "Bong" : "Ping";
+            message.setContent(messageContent);
+            send(message);
+        }
+    }
+}
---
+++ src/src/B/BClass.java
@@ -0,0 +1,34 @@
+package B;
+
+import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
+
+import java.util.Objects;
+
+public class BClass extends Agent {
+
+    @Override
+    protected void setup() {
+        System.out.println("Agent B is ready too: " + getAID().getName() + "Let`s go");
+        addBehaviour(new CyclicBehaviour(this) {
+            @Override
+            public void action() {
+                ACLMessage receivedMessage = receive();
+                if (receivedMessage != null) {
+                    System.out.println(" - " + myAgent.getLocalName() + " received: " + receivedMessage.getContent());
+                    ACLMessage reply = receivedMessage.createReply();
+                    int perform = ACLMessage.NOT_UNDERSTOOD;
+                    String msg = "I`m not understood";
+                    if (Objects.equals(receivedMessage.getContent(), "Ping")) {
+                        perform = ACLMessage.INFORM;
+                        msg = "Pong";
+                    }
+                    reply.setContent(msg);
+                    send(reply);
+                }
+                block();
+            }
+        });
+    }
+}
